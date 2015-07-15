c423a510d54e652ca214ef61c1a609d97f28d90c
jan fajfr <jan@rfq-hub.com>
2015-07-15 09:27:14 +0200

    extracting the script for elasticsearch into separate file

---
+++ run_es.ps1
@@ -0,0 +1,31 @@
+Invoke-WebRequest "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.6.0.zip" -OutFile .\es16.zip;
+
+$destFolder = "es16\elasticsearch-1.6.0";
+
+$shell = new-object -com shell.application;
+
+
+$zip = $shell.NameSpace("$pwd\es16.zip");
+
+if (Test-Path $pwd\$destFolder )
+
+{
+	del $pwd\$destFolder -Force -Recurse
+}
+
+md ".\es16";
+
+foreach($item in $zip.items())
+
+{
+	$shell.Namespace("$pwd\es16").copyhere($item);
+}
+
+cd $destFolder	
+
+.\bin\service.bat install
+
+.\bin\service.bat start
+
+cd ..
+cd ..
