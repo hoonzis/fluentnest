6c01ff3eb789858b212228ab2607bf38bb1889da
Jan Fajfr <hoonzis@gmail.com>
2016-09-25 11:05:17 +0200

    first take on consequent range filters merged into single range

--- FluentNest/Filters.cs
+++ FluentNest/Filters.cs
@@ -243,6 +243,55 @@ namespace FluentNest
             return FirstCharacterToLower(expression.Member.Name);
         }
 
+        public static bool IsComparisonType(this ExpressionType expType)
+        {
+            return expType == ExpressionType.LessThan || expType == ExpressionType.GreaterThan || expType == ExpressionType.LessThanOrEqual || expType == ExpressionType.GreaterThanOrEqual;
+        }
+
+        public static string GetFieldName(this Expression exp)
+        {
+            var binary = (BinaryExpression)exp;
+            var memberAccessor = binary.Left as MemberExpression;
+            var fieldName = GetFieldNameFromMember(memberAccessor);
+            return fieldName;
+        }
+
+        public static QueryContainer GenerateRangeFilter<T>(String fieldName, Object leftValue, ExpressionType leftType, Object rightValue, ExpressionType rightType)
+            where T : class
+        {
+            if (leftValue is DateTime)
+            {
+                var leftDate = (DateTime)leftValue;
+                var rightDate = (DateTime)rightValue;
+                var filterDescriptor = new QueryContainerDescriptor<T>();
+                return filterDescriptor.DateRange(x => x.RangeOnDate(leftType, leftDate).RangeOnDate(rightType, rightDate).Field(fieldName));
+            }
+
+            throw new NotImplementedException();
+            
+        }
+
+        public static DateRangeQueryDescriptor<T> RangeOnDate<T>(this DateRangeQueryDescriptor<T> filterDescriptor, ExpressionType type, DateTime value) where T : class
+        {
+            if (type == ExpressionType.LessThan)
+            {
+                return filterDescriptor.LessThan(value);
+            }
+            if (type == ExpressionType.GreaterThan)
+            {
+                return filterDescriptor.GreaterThan(value);
+            }
+            if (type == ExpressionType.LessThanOrEqual)
+            {
+                return filterDescriptor.LessThanOrEquals(value);
+            }
+            if (type == ExpressionType.GreaterThanOrEqual)
+            {
+                return filterDescriptor.GreaterThanOrEquals(value);
+            }
+            throw new NotImplementedException();
+        }
+
         public static QueryContainer GenerateFilterDescription<T>(this Expression expression) where T:class
         {
             var expType = expression.NodeType;
@@ -250,6 +299,24 @@ namespace FluentNest
             if (expType == ExpressionType.AndAlso)
             {
                 var binaryExpression = (BinaryExpression)expression;
+
+                // handle special cases of two comparisons on the same field which should be compiled into a range request
+                if (binaryExpression.Left.NodeType.IsComparisonType() && binaryExpression.Right.NodeType.IsComparisonType())
+                {
+                    if(binaryExpression.Left.GetFieldName() == binaryExpression.Right.GetFieldName())
+                    {
+                        var leftBinary = (BinaryExpression)expression;
+                        var leftValue = GetValue(leftBinary);
+                        
+                        var memberAccessor = leftBinary.Left as MemberExpression;
+                        var fieldName = GetFieldNameFromMember(memberAccessor);
+
+                        var rightBinary = (BinaryExpression)expression;
+                        var rightValue = GetValue(rightBinary);
+                        GenerateRangeFilter<T>(fieldName, leftValue, leftBinary.NodeType, rightValue, rightBinary.NodeType);
+                    }
+                }
+                
                 var leftFilter = GenerateFilterDescription<T>(binaryExpression.Left);
                 var rightFilter = GenerateFilterDescription<T>(binaryExpression.Right);
                 var filterDescriptor = new QueryContainerDescriptor<T>();
